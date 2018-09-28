0a97da2e91226a06b4a8b3f8e888d52337b5071d
Vladislav Ishchenko <vladis.ish@gmail.com>
2018-09-28 10:56:04 +0300

    m2composed

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -24,7 +24,7 @@ class ComposerAutoloaderInit4dbd6d03a2e4327b3d257d87f16e3425
         spl_autoload_unregister(array('ComposerAutoloaderInit4dbd6d03a2e4327b3d257d87f16e3425', 'loadClassLoader'));
 
         $includePaths = require __DIR__ . '/include_paths.php';
-        array_push($includePaths, get_include_path());
+        $includePaths[] = get_include_path();
         set_include_path(implode(PATH_SEPARATOR, $includePaths));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
