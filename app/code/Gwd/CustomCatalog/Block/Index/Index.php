b9ba5c86cd6332816000a1c101e061ecb457c30b
Vladislav Ishchenko <vladis.ish@gmail.com>
2018-10-03 13:52:56 +0300

    api2

--- app/code/Gwd/CustomCatalog/Block/Index/Index.php
+++ app/code/Gwd/CustomCatalog/Block/Index/Index.php
@@ -5,17 +5,23 @@ namespace Gwd\CustomCatalog\Block\Index;
 
 class Index extends \Magento\Framework\View\Element\Template
 {
+    protected $_productCollectionFactory;
 
-    /**
-     * Constructor
-     *
-     * @param \Magento\Framework\View\Element\Template\Context  $context
-     * @param array $data
-     */
     public function __construct(
-        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Backend\Block\Template\Context $context,
+        \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory,
         array $data = []
-    ) {
+    )
+    {
+        $this->_productCollectionFactory = $productCollectionFactory;
         parent::__construct($context, $data);
+    }
+
+    public function getProductCollection()
+    {
+        $collection = $this->_productCollectionFactory->create();
+        $collection->addAttributeToSelect('*');
+        $collection->setPageSize(3); // fetching only 3 products
+        return $collection;
     }
 }
--- app/code/Gwd/CustomCatalog/Model/TestapiManagement.php
+++ app/code/Gwd/CustomCatalog/Model/TestapiManagement.php
@@ -5,13 +5,56 @@ use Gwd\CustomCatalog\Api\TestapiManagementInterface;
 
 class TestapiManagement implements TestapiManagementInterface
 {
-    /**
-     * Returns greeting message to user
-     *
-     * @api
-     * @param string $name Users name.
-     * @return string Greeting message with users name.
-     */
+
+
+
+
+
+
+    public function name($name)
+    {
+
+        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+
+        $productCollection = $objectManager->create('Magento\Catalog\Model\ResourceModel\Product\CollectionFactory');
+
+        $collection = $productCollection->create()
+            ->addAttributeToSelect('*')
+            ->addAttributeToFilter(
+                   "sku", array("eq" => "$name"))
+            ->load();
+        echo $collection->getSelect();
+//        foreach ($collection as $product){
+//            echo 'Name  =  '.$product->getName().'<br>';
+//        }
+////
+//        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+//        $collection = $objectManager->create('Magento\Catalog\Model\ResourceModel\Product\Collection');
+//        $productcollection =
+//            $collection->getCollection()->addAttributeToSelect('*')
+//                ->addAttributeToFilter(
+//                    "sku", array("eq" => "$name")
+//                )->load();
+//
+//
+//
+//        echo $productcollection->getSelect();
+
+    }
+
+}
+
+/*
+$model = $this->_objectManager->create(\Magento\Catalog\Model\Product::class);
+$model->load($name);
+if ($name) {
+    foreach ($model as $sku){
+        echo 'SKU ='.$sku->getsku().'<br>';
+    }
+
+}
+return "Hello, " . $name;*/
+
 //    public function name($name)
 //    {
 //
@@ -26,85 +69,5 @@ class TestapiManagement implements TestapiManagementInterface
 //        }
 //
 //    }
-
-
-    /**
-     * @var \Magento\Framework\Api\SearchCriteriaBuilder
-     */
-    protected $searchCriteriaBuilder;
-
-    /**
-     * @var \Magento\Catalog\Api\ProductRepositoryInterface
-     */
-    protected $productRepository;
-
-    /**
-     * @var \Magento\Framework\Api\FilterBuilder
-     */
-    protected $filterBuilder;
-
-    /**
-     * @var \Magento\Framework\Api\Search\FilterGroup
-     */
-    protected $filterGroup;
-
-    public function __construct(
-        \Magento\Catalog\Api\ProductRepositoryInterface $productRepository,
-        \Magento\Framework\Api\SearchCriteriaInterface $searchCriteria,
-        \Magento\Framework\Api\FilterBuilder $filterBuilder,
-        \Magento\Framework\Api\Search\FilterGroup $filterGroup)
-    {
-        $this->productRepository = $productRepository;
-        $this->searchCriteria = $searchCriteria;
-        $this->filterBuilder = $filterBuilder;
-        $this->filterGroup = $filterGroup;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function name($name)
-    {
-        $this->filterGroup->setFilters([
-            $this->filterBuilder->setField('url_key')->setConditionType('eq')
-                ->setValue($name)->create()]);
-        $this->searchCriteria->setFilterGroups([$this->filterGroup]);
-        $products = $this->productRepository->getList($this->searchCriteria);
-        if (count($products) == 0) {
-            return null;
-        }
-        $items = $products->getItems();
-        foreach ($items as $item) {
-            return $item;
-        }
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-//    public function getProductById($id)
-//    {
-//        return $this->productRepository->getById($id);
-//    }
-
-}
-
-/**
- *
- *
- *
- *
- *
- *
- */
-/*
-$model = $this->_objectManager->create(\Magento\Catalog\Model\Product::class);
-$model->load($name);
-if ($name) {
-    foreach ($model as $sku){
-        echo 'SKU ='.$sku->getsku().'<br>';
-    }
-
-}
-return "Hello, " . $name;*/
+//
 
