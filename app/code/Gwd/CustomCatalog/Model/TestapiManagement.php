0c607d1d28ffa4ad2005c845601d4d67a4665e51
moonwheel <dimapetrenko.in@gmail.com>
2018-10-18 19:06:11 +0300

    v. 3

--- app/code/Gwd/CustomCatalog/Model/TestapiManagement.php
+++ app/code/Gwd/CustomCatalog/Model/TestapiManagement.php
@@ -6,11 +6,6 @@ use Gwd\CustomCatalog\Api\TestapiManagementInterface;
 class TestapiManagement implements TestapiManagementInterface
 {
 
-
-
-
-
-
     public function name($name)
     {
 
@@ -23,51 +18,11 @@ class TestapiManagement implements TestapiManagementInterface
             ->addAttributeToFilter(
                    "sku", array("eq" => "$name"))
             ->load();
+
         echo $collection->getSelect();
-//        foreach ($collection as $product){
-//            echo 'Name  =  '.$product->getName().'<br>';
-//        }
-////
-//        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-//        $collection = $objectManager->create('Magento\Catalog\Model\ResourceModel\Product\Collection');
-//        $productcollection =
-//            $collection->getCollection()->addAttributeToSelect('*')
-//                ->addAttributeToFilter(
-//                    "sku", array("eq" => "$name")
-//                )->load();
-//
-//
-//
-//        echo $productcollection->getSelect();
 
+        die;
     }
-
 }
 
-/*
-$model = $this->_objectManager->create(\Magento\Catalog\Model\Product::class);
-$model->load($name);
-if ($name) {
-    foreach ($model as $sku){
-        echo 'SKU ='.$sku->getsku().'<br>';
-    }
-
-}
-return "Hello, " . $name;*/
-
-//    public function name($name)
-//    {
-//
-//        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-//
-//        $productCollection = $objectManager->create('Magento\Catalog\Model\ResourceModel\Product\CollectionFactory');
-//
-//        $collection = $productCollection->create()->load($name);
-//
-//        foreach ($collection as $product) {
-//            echo 'SKU  =  ' . $product->getSku() . '<br>';
-//        }
-//
-//    }
-//
 
